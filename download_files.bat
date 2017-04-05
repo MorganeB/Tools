006afdfb923d223e7b1f7251d1e5c7efefcdb69d
MorganeB <valgrin123@gmail.com>
2017-04-05 21:48:25 +0200

    change to dummy url

--- download_files.bat
+++ download_files.bat
@@ -1,15 +1,11 @@
 @echo off
 
-call:function_download "https://1fichier.com/?rdgyidjqds"
-call:function_download "https://1fichier.com/?hpmqjlldr3"
-call:function_download "https://1fichier.com/?kyq4nxnlmo"
-call:function_download "https://1fichier.com/?v4uklqaa8w"
-call:function_download "https://1fichier.com/?0x2d0d1j5d"
-call:function_download "https://1fichier.com/?u4slybpowq"
-call:function_download "https://1fichier.com/?a2qcc96ucy"
-call:function_download "https://1fichier.com/?pva3mn063s"
-call:function_download "https://1fichier.com/?asezgvlgb0"
-call:function_download "https://1fichier.com/?cw9qvx6doq"
+call:function_download "https://1fichier.com/?xxxxx"
+call:function_download "https://1fichier.com/?xxxxx"
+call:function_download "https://1fichier.com/?xxxxx"
+call:function_download "https://1fichier.com/?xxxxx"
+call:function_download "https://1fichier.com/?xxxxx"
+
                                                         
 echo.&goto:eof
 
